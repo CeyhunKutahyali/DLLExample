28aa381d9517d8f4edf0e67626bb628e33623869
Ceyhun KUTAHYALI <ceyhun.kutahyali@gmail.com>
2023-11-30 13:54:42 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace DLLExample
+{
+    public class Calculator
+    {
+        public static int squareCalculate(int number)
+        {
+            return (number * number);
+        }
+
+        public static int cubeCalculate(int number)
+        {
+            return (number * number * number);
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLExample
+{
+    public class Rectangle
+    {
+        private int shortSide;
+        private int longSide;
+
+        public int ShortSide { get => shortSide; set => shortSide = value; }
+        public int LongSide { get => longSide; set => longSide = value; }
+
+
+        public Rectangle(int shortSide, int longSide)
+        {
+            this.shortSide = shortSide;
+            this.longSide = longSide;
+        }
+
+        public int primeterCalculate()
+        {
+            return (2 * (shortSide * longSide));
+        }
+
+        public int areaCalculate()
+        {
+            return (shortSide * longSide);
+        }
+    }
+}
